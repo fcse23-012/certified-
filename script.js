8dd024dedc0deeedaa014caf58230f6d8cf7cf8e
fcse23-012 <fcse23-012@thuto.bac.ac.bw>
2025-03-01 14:39:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+      function leftScroll() {
+        const left = document.querySelector(".scroll-images");
+        left.scrollBy(200, 0);
+      }
+      function rightScroll() {
+        const right = document.querySelector(".scroll-images");
+        right.scrollBy(-200, 0);
+      }
+</script>
